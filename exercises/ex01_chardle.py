a0223b2556210fda579f8eb28a67e77e5480377a
Yujun Ming <yujun.ming@unc.edu>
2022-01-19 18:23:16 -0500

    Add no instance

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,20 +3,23 @@
 __author__ = "9842615731"
 
 word: str=input("Enter a 5-character word:")
-ch: str=input("Enter a single character:")
-count: int=0
-print("Searching for e in hello")
 if(len(word)!=5):
     print("Error: Word must contain 5 characters")
     quit()
+ch: str=input("Enter a single character:")
 if(len(ch)!=1):
     print("Error: Character must be a single character.")
     quit()
+count: int=0
+print("Searching for e in hello")
 for x in range(5):
     if(word[x]==ch):
         print(ch+" found at index "+str(x))
         count=count+1
-print(str(count)+" instance of "+ch+" found in "+word)
+if count==0:
+    print("No instance of "+ch+" found in "+word)
+else:
+    print(str(count)+" instance of "+ch+" found in "+word)
 
 
 
