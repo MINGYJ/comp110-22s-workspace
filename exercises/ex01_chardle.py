13803b97a90a46301ef5d8bad4ce143ac600242c
MINGYJ <myjsteven@outlook.coml>
2022-01-23 22:06:20 -0500

    add one

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,36 +1,38 @@
 """EX01 - Chardle - A cute step toward Wordle."""
+__author__ = "730475325"
 
-__author__ = "730486611"
-
-word: str = input("Enter a 5-character word: ")
-if(len(word) != 5):
-    print("Error: Word must contain 5 characters")
-    quit()
-ch: str = input("Enter a single character: ")
-if(len(ch) != 1):
+word: str = input("Enter a 5-character word ")
+if len(word) != 5:
+    print("Error: Word must contain 5 characters") 
+    exit()  
+character: str = input("Enter a single character ")
+if len(character) != 1:
     print("Error: Character must be a single character.")
-    quit()
-count: int = 0
-print("Searching for " + ch + " in " + word)
-if(word[0] == ch):
-    print(ch + " found at index " + str(0))
+    exit()
+count: int
+count = 0
+print("searching for " + character + " in " + word)
+
+if character == word[0]: 
+    print(character + " found at index 0")
     count = count + 1
-if(word[1] == ch):
-    print(ch + " found at index " + str(1))
+if character == word[1]:
+    print(character + " found at index 1")
     count = count + 1
-if(word[2] == ch):
-    print(ch + " found at index " + str(2))
+if character == word[2]:
+    print(character + " found at index 2")
     count = count + 1
-if(word[3] == ch):
-    print(ch + " found at index " + str(3))
+if character == word[3]:
+    print(character + " found at index 3")
     count = count + 1
-if(word[4] == ch):
-    print(ch + " found at index " + str(4))
+if character == word[4]:
+    print(character + " found at index 4")
     count = count + 1
+
 if count == 0:
-    print("No instances of " + ch + " found in " + word)
+    print("No instances of " + character + " found in " + word)
 else:
-    if(count == 1):
-        print(str(count) + " instance of " + ch + " found in " + word)
+    if count == 1:
+        print("1 instance of " + character + " found in " + word)
     else:
-        print(str(count) + " instances of " + ch + " found in " + word)
+        print(count, "instances of " + character + " found in " + word)
---
+++ exercises/ex01_chardlemyj.py
@@ -0,0 +1,36 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730486611"
+
+word: str = input("Enter a 5-character word: ")
+if(len(word) != 5):
+    print("Error: Word must contain 5 characters")
+    quit()
+ch: str = input("Enter a single character: ")
+if(len(ch) != 1):
+    print("Error: Character must be a single character.")
+    quit()
+count: int = 0
+print("Searching for " + ch + " in " + word)
+if(word[0] == ch):
+    print(ch + " found at index " + str(0))
+    count = count + 1
+if(word[1] == ch):
+    print(ch + " found at index " + str(1))
+    count = count + 1
+if(word[2] == ch):
+    print(ch + " found at index " + str(2))
+    count = count + 1
+if(word[3] == ch):
+    print(ch + " found at index " + str(3))
+    count = count + 1
+if(word[4] == ch):
+    print(ch + " found at index " + str(4))
+    count = count + 1
+if count == 0:
+    print("No instances of " + ch + " found in " + word)
+else:
+    if(count == 1):
+        print(str(count) + " instance of " + ch + " found in " + word)
+    else:
+        print(str(count) + " instances of " + ch + " found in " + word)
